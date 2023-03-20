4fdee52a0a14477f80454a6246bd7b9cbc7c9207
pzprado <pete.zprado@gmail.com>
2023-03-20 16:44:41 +1100

    fixing angular files

--- frontend-angular/src/app/app.component.ts
+++ frontend-angular/src/app/app.component.ts
@@ -1,30 +1,80 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { ethers } from 'ethers';
+import { BigNumber, Contract, Wallet, ethers, utils } from 'ethers';
+import tokenJson from '../assets/MyToken.json';
+
+const TOKEN_ADDRESS_API_URL = 'http://localhost:3000/contract-address';
+const TOKEN_MINT_API_URL = 'http://localhost:3000/request-tokens';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   blockNumber: number | string | undefined;
-  transactions: string[] | undefined;
+  provider: ethers.providers.BaseProvider;
+  userWallet: Wallet | undefined;
+  userBalance: number | undefined;
+  userTokenBalance: number | undefined;
+  tokenContractAddress: string | undefined;
+  tokenContract: Contract | undefined;
+  tokenSupply: number | undefined;
 
-  constructor() {
-    
+  constructor(private http: HttpClient) {
+    this.provider = ethers.providers.getDefaultProvider('goerli');
   }
 
   syncBlock() {
     this.blockNumber = 'Loading...';
-    const provider = ethers.providers.getDefaultProvider('goerli');
-    provider.getBlock('latest').then((block) => {
+    this.provider.getBlock('latest').then((block) => {
       this.blockNumber = block.number;
-      this.transactions = block.transactions;
-    })
+    });
+    this.http
+      .get<{ result: string }>(TOKEN_ADDRESS_API_URL)
+      .subscribe((answer) => {
+        this.tokenContractAddress = answer.result;
+        this.getTokenInfo();
+      });
+  }
+
+  getTokenInfo() {
+    if (!this.tokenContractAddress) return;
+    this.tokenContract = new Contract(
+      this.tokenContractAddress,
+      tokenJson.abi,
+      this.userWallet ?? this.provider
+    );
+    this.tokenContract['totalSupply']().then((totalSupplyBN: BigNumber) => {
+      const totalSupplyStr = utils.formatEther(totalSupplyBN);
+      this.tokenSupply = parseFloat(totalSupplyStr);
+    });
   }
 
   clearBlock() {
     this.blockNumber = undefined;
-    this.transactions = undefined;
+  }
+
+  createWallet() {
+    this.userWallet = Wallet.createRandom().connect(this.provider);
+    this.userWallet.getBalance().then((balanceBN) => {
+      const balanceStr = utils.formatEther(balanceBN);
+      this.userBalance = parseFloat(balanceStr);
+      this.tokenContract?.['balanceOf'](this.userWallet?.address).then(
+        (tokenBalanceBN: BigNumber) => {
+          const tokenBalanceStr = utils.formatEther(tokenBalanceBN);
+          this.userTokenBalance = parseFloat(tokenBalanceStr);
+        }
+      );
+    });
+  }
+
+  requestTokens(value: string) {
+    const body = { address: this.userWallet?.address, value: value };
+    this.http
+      .post<{ result: any }>(TOKEN_MINT_API_URL, body)
+      .subscribe((ans) => {
+        console.log({ ans });
+      });
   }
 }
